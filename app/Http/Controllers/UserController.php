2585c05a019ab46b060132974008c9668675357c
Franklin3004 <81929967+Franklin3004@users.noreply.github.com>
2021-04-23 21:22:17 -0500

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+class UserController extends Controller
+{
+    public function index(){
+        //select * from users
+        //$users = User::all();
+        $users = User::paginate(5);
+        return view('User.index', compact('users'));
+    }
+}
